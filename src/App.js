ccce20aa201802a1a275bb9c99cab7db76fa3ac5
DivyaniM <divyani25494@gmail.com>
2021-06-30 04:40:04 +0530

    broken all the homepage componentand commit after homepage creation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.componet/homepage.component';
+import HomePage from './pages/homepage.component/homepage.component';
 
 function App() {
   return (
